505b241c37e3f0e7043c38622944151630b69876
Yuval Shavit <yankee.sierra@gmail.com>
2013-05-14 22:20:06 -0400

    manual executor-like service

--- ProfileBreaker.java
+++ ProfileBreaker.java
@@ -101,30 +101,92 @@ public class ProfileBreaker implements Callable<Result> {
         return ints;
     }
 
-    public static void main(String[] args) throws Exception {
-        int nTasks = Integer.parseInt(args[0]);
-        int iters = Integer.parseInt(args[1]);
-        int arraySize = Integer.parseInt(args[2]);
-        boolean verbose = Boolean.parseBoolean(args[3]);
+    public static class Handler implements Runnable {
+        private final BlockingQueue<ProfileBreaker> workQueue;
+        private final AtomicInteger resultInt;
+        private final CountDownLatch doneLatch;
+        private final boolean verbose;
 
-        SimpleRand rand = new SimpleRand((int)System.nanoTime());
-        Queue<Future<Result>> results = new ArrayDeque<Future<Result>>(nTasks);
-        ExecutorService executor = Executors.newFixedThreadPool(4);
-
-        for (int i = 0; i < nTasks; ++i) {
-            Future<Result> future = executor.submit(new ProfileBreaker(rand.nextInt(), iters, arraySize));
-            results.add(future);
+        public Handler(BlockingQueue<ProfileBreaker> workQueue, AtomicInteger resultInt, CountDownLatch doneLatch, boolean verbose) {
+            this.workQueue = workQueue;
+            this.resultInt = resultInt;
+            this.doneLatch = doneLatch;
+            this.verbose = verbose;
         }
-        int resultSum = 1;
-        while (!results.isEmpty()) {
-            Future<Result> future = results.remove();
-            Result result = future.get();
-            resultSum += result.value;
-            if (verbose) {
-                System.out.println(result);
+        
+        @Override
+        public void run() {
+            while(true) {
+                ProfileBreaker breaker;
+                try {
+                    breaker = workQueue.take();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+                Result result = breaker.call();
+                if (verbose) {
+                    message(result);
+                }
+                resultInt.addAndGet(result.value);
+                doneLatch.countDown();
             }
         }
+    }
+
+    public static class Producer implements Runnable {
+        private final BlockingQueue<ProfileBreaker> workQueue;
+        private final int iters;
+        private final int arraySize;
+
+        public Producer(BlockingQueue<ProfileBreaker> workQueue, int iters, int arraySize) {
+            this.workQueue = workQueue;
+            this.iters = iters;
+            this.arraySize = arraySize;
+        }
+        
+        @Override
+        public void run() {
+            SimpleRand rand = new SimpleRand((int)System.nanoTime() + hashCode());
+            while (true) {
+                try {
+                    workQueue.put(new ProfileBreaker(rand.nextInt(), iters, arraySize));
+                }
+                catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return; // Done
+                }
+            }
+        }
+    }
+        
+    public static synchronized void message(Object message) {
+        System.out.println(String.valueOf(message));
+    }
+
+    public static void main(String[] args) throws Exception {
+        final int nTasks = Integer.parseInt(args[0]);
+        final int iters = Integer.parseInt(args[1]);
+        final int arraySize = Integer.parseInt(args[2]);
+        final boolean verbose = Boolean.parseBoolean(args[3]);
+        
+        int nThreads = Integer.parseInt(System.getProperty("threads", "2"));
+
+        final BlockingQueue<ProfileBreaker> breakers = new ArrayBlockingQueue<ProfileBreaker>(nThreads * 10);
+        final AtomicInteger resultSum = new AtomicInteger(0);
+        final CountDownLatch doneLatch = new CountDownLatch(nTasks);
+        for (int i = 0; i < nThreads; ++i) {
+            startThread(new Producer(breakers, iters, arraySize));
+            startThread(new Handler(breakers, resultSum, doneLatch, verbose));
+        }
+        
+        doneLatch.await();
         System.out.println(resultSum);
-        executor.shutdown();
+    }
+
+    private static void startThread(Runnable runnable) {
+        Thread t = new Thread(runnable);
+        t.setDaemon(true);
+        t.start();
     }
 }
