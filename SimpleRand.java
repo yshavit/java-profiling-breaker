ab93a316b24e37f29d6a905c913bcd546eadf440
Yuval Shavit <yankee.sierra@gmail.com>
2013-05-14 20:43:11 -0400

    rename NoSyncRand, provide a static helper for it

--- SimpleRand.java
+++ SimpleRand.java
@@ -1,12 +1,16 @@
-public class NoSyncRand {
+public class SimpleRand {
     private int v;
     
-    public NoSyncRand(int seed) {
+    public SimpleRand(int seed) {
         v = seed;
         nextInt();
     }
 
     public int nextInt() {
+        return (v = nextInt(v));
+    }
+
+    public static int nextInt(int v) {
         // Taken from JCIP 12.1.3 (listing 12.4): http://jcip.net 
         v ^= v << 6;
         v ^= v >>> 21;
